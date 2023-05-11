71613e61b4600ab5926ec1c282789ff0483ccbcf
jihyunglee <jhlee7125@gmail.com>
2023-05-11 13:23:40 +0900

    기본적인 redux 설정 - createStore, reducer

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,18 +1,15 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-
-const handleAdd = () => {
-  count++;
-  number.innerText = count;
+// reducer의 역할을 countModifier가 대신한다.
+const countModifier = () => {
+  return 'hello';
 };
+// countStore는 count 데이터를 저장하고 있는 장소이다.
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count--;
-  number.innerText = count;
-};
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+// getState는 countStore에 저장된 데이터를 가져온다.
+console.log(countStore.getState());
