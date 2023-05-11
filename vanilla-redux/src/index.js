8546ffc649bd10fcad88e89b447fb51976dcee25
jihyunglee <jhlee7125@gmail.com>
2023-05-11 13:33:11 +0900

    reducer에 동작할 메시지의 형태를 전달하는 방법

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -6,12 +6,12 @@ const number = document.querySelector('span');
 
 // reducer의 역할을 countModifier가 대신한다.
 // state의 default를 0으로 설정한다.
-const countModifier = (state = 0) => {
-  // ... modify state code ...
-  return state;
+// state는 곧 count에 해당한다.
+const countModifier = (count = 0, action) => {
+  console.log('action', action); // { type: 'HELLO' }
+  return count;
 };
 // countStore는 count 데이터를 저장하고 있는 장소이다.
 const countStore = createStore(countModifier);
 
-// getState는 countStore에 저장된 데이터를 가져온다.
-console.log(countStore.getState()); // 0 <- by default setting of state
+countStore.dispatch({ type: 'HELLO' }); // -> the way to send message to reducer
