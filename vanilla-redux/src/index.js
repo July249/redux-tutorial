5b731b25107e8b1b77a27802d2f4148cd1b56f37
jihyunglee <jhlee7125@gmail.com>
2023-05-11 13:26:26 +0900

    state의 변경은 reducer에서 해당 로직을 담당한다.

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -5,11 +5,13 @@ const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
 // reducer의 역할을 countModifier가 대신한다.
-const countModifier = () => {
-  return 'hello';
+// state의 default를 0으로 설정한다.
+const countModifier = (state = 0) => {
+  // ... modify state code ...
+  return state;
 };
 // countStore는 count 데이터를 저장하고 있는 장소이다.
 const countStore = createStore(countModifier);
 
 // getState는 countStore에 저장된 데이터를 가져온다.
-console.log(countStore.getState());
+console.log(countStore.getState()); // 0 <- by default setting of state
