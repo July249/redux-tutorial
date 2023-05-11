d1575bf4dcc3d95ef0f59bc108e58b729902da0a
jihyunglee <jhlee7125@gmail.com>
2023-05-11 14:24:48 +0900

    subscribe를 이용하여 인자로 전달된 argument 함수를 동작시킨다. 해당 함수의 동작을 통해 number의 값을 최신화 시켜준다.

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -5,21 +5,27 @@ const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
 const countModifier = (count = 0, action) => {
+  // 하기의 if문 안에서는 textContent를 직접적으로 count로 갱신하고 있다.
+  // 하지만 subscribe를 이용하면 최신의 count 값을 가져오게 할 수 있다.
+  // 매번 if 문 안에 return (number.textContent = count); 를 적어주는 것은 번거롭다.
   if (action.type === 'ADD') {
-    count++;
-    return (number.textContent = count);
+    return count + 1;
+    // count++;
+    // return (number.textContent = count);
   } else if (action.type === 'MINUS') {
-    count--;
-    return (number.textContent = count);
+    return count - 1;
+    // count--;
+    // return (number.textContent = count);
   } else {
-    return (number.textContent = count);
+    return count;
+    // return (number.textContent = count);
   }
 };
 const countStore = createStore(countModifier);
 
 const handleChange = () => {
-  // 버튼이 클릭될 때마다 로그가 출력된다.
-  console.log('there was a change on the store');
+  console.log(countStore.getState()); // 버튼이 클릭될 때마다 store의 상태를 출력한다.
+  number.textContent = countStore.getState(); // store의 상태를 가져와서 textContent를 갱신한다.
 };
 
 // subscribe는 store의 변화를 감지한다.
