3bb5edddf17b08062a41c293df8d923097f30126
jihyunglee <jhlee7125@gmail.com>
2023-05-11 14:19:46 +0900

    subscribe의 기본 동작사항 확인

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -17,5 +17,14 @@ const countModifier = (count = 0, action) => {
 };
 const countStore = createStore(countModifier);
 
+const handleChange = () => {
+  // 버튼이 클릭될 때마다 로그가 출력된다.
+  console.log('there was a change on the store');
+};
+
+// subscribe는 store의 변화를 감지한다.
+// 해당 변화가 감지되면 subscribe에 등록된 함수를 실행한다.
+countStore.subscribe(handleChange);
+
 add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
 minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
