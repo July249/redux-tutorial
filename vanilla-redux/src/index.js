f0c974de0e94bbe2916a7e432340e3d93c247eea
jihyunglee <jhlee7125@gmail.com>
2023-05-11 14:08:05 +0900

    dispatch로 countStore의 count 값을 조정한다. 이 때 type에 따라 countModifier에서 count 값이 변경된다. count 값의 유일한 변경점은 countModifier 이여야 한다.

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -8,10 +8,22 @@ const number = document.querySelector('span');
 // state의 default를 0으로 설정한다.
 // state는 곧 count에 해당한다.
 const countModifier = (count = 0, action) => {
-  console.log('action', action); // { type: 'HELLO' }
-  return count;
+  // action.type에 따라 처리해야할 동작사항에 달라진다.
+  // action.type으로 ADD를 전달한다.
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 // countStore는 count 데이터를 저장하고 있는 장소이다.
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: 'HELLO' }); // -> the way to send message to reducer
+countStore.dispatch({ type: 'ADD' }); // countStore에 type을 전달하는 역할을 dispatch가 한다.
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
+
+console.log(countStore.getState()); // 2가 출력되는 것을 확인할 수 있다.
